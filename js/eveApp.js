aca22b37d3f89241285b937c06bc865fb9b5958b
Valentino Ekaputra <silentrazgriz@gmail.com>
2016-12-29 15:05:40 +0700

    progressbar added
    TODO: link progress variable to requestMarketOrders

--- js/eveApp.js
+++ js/eveApp.js
@@ -22,12 +22,14 @@ class EveApp {
 					groups: eveGroups,
 					categories: eveCategories
 				},
-				orderData: [],
+				progress: 0,
+				orderData: []
 			},
 			methods: {
 				findOrders: function(e) {
 					e.preventDefault();
 					this.orderData = [];
+					this.progress = 0;
 					let api = new ApiAccess(this.$http, this.orderData);
 					api.requestMarketOrders(this.config, 0);
 				}
